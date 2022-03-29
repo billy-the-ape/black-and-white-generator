41f9235eb3be93d534479380bbcbcbfc67f67b07
Wack Overflow cryptobilly37@gmail.com <cryptobilly37@gmail.com>
2022-03-28 21:43:11 -0600

    Basic initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {ThemeProvider, createTheme, CssBaseline} from '@mui/material';
+
+export const darkTheme = createTheme({ palette: { mode: 'dark' } });
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
